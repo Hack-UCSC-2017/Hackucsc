418a434fa44370de8eeeb58def07d37f6891cac3
Will Bolden <WilliamHBolden@gmail.com>
2017-01-21 11:34:12 -0800

    Get screen res/dimensions

--- app/src/main/java/com/SteadyView/SteadyView/MainActivity.java
+++ app/src/main/java/com/SteadyView/SteadyView/MainActivity.java
@@ -1,19 +1,32 @@
 package com.SteadyView.SteadyView;
 
 import android.content.Context;
+import android.graphics.Point;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Display;
 import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+import java.lang.reflect.Method;
+
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
     private SensorManager senSensorManager;
     private Sensor senAccelerometer;
+    private long lastSensorTimestamp = -1;
+    int width = 1080;
+    int height = 1920;
+
+    double widthMeters;
+    double heightMeters;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +41,24 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         web.setWebViewClient(new WebViewClient());
         web.loadUrl("http://reddit.com");
 
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getRealSize(size);
+        width = size.x;
+        height = size.y;
+
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        widthMeters = 0.0254*(double)width/metrics.xdpi;
+        heightMeters = 0.0254*(double)height/metrics.ydpi;
+
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        System.out.println(event.values[0] + ","+event.values[1]+","+event.values[2]);
+        //System.out.println(event.values[0] + ","+event.values[1]+","+event.values[2]);
+
+        lastSensorTimestamp = event.timestamp;
     }
 
     @Override
