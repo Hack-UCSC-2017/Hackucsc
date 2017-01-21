d9c2ad929e5658bdb9dfe178fe5781ffc4ceb899
Will Bolden <WilliamHBolden@gmail.com>
2017-01-21 12:56:33 -0800

    Semi-working stabilization

--- app/src/main/java/com/SteadyView/SteadyView/MainActivity.java
+++ app/src/main/java/com/SteadyView/SteadyView/MainActivity.java
@@ -22,21 +22,28 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private SensorManager senSensorManager;
     private Sensor senAccelerometer;
     private long lastSensorTimestamp = -1;
+
+    WebView web;
+
     int width = 1080;
     int height = 1920;
 
     double widthMeters;
     double heightMeters;
 
+    double xdpm = 1;
+    double ydpm = 1;
+
+    double[] v = {0,0};
+    double[] p = {0,0};
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        senSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        senAccelerometer = senSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
-        senSensorManager.registerListener(this, senAccelerometer , SensorManager.SENSOR_DELAY_FASTEST);
 
-        WebView web = (WebView)findViewById(R.id.webview);
+        web = (WebView)findViewById(R.id.webview);
         web.getSettings().setJavaScriptEnabled(true);
         web.setWebViewClient(new WebViewClient());
         web.loadUrl("http://reddit.com");
@@ -51,12 +58,43 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         getWindowManager().getDefaultDisplay().getMetrics(metrics);
         widthMeters = 0.0254*(double)width/metrics.xdpi;
         heightMeters = 0.0254*(double)height/metrics.ydpi;
+        xdpm = metrics.xdpi/0.0254;
+        ydpm = metrics.ydpi/0.0254;
+
+        //float inchpix = (float) (metrics.xdpi);
+        //web.setX(inchpix);
+
+        senSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        senAccelerometer = senSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+        senSensorManager.registerListener(this, senAccelerometer , SensorManager.SENSOR_DELAY_FASTEST);
+
 
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        //System.out.println(event.values[0] + ","+event.values[1]+","+event.values[2]);
+        System.out.println(event.values[0] + ","+event.values[1]+","+event.values[2]);
+        //0 = x, 1 = y, 2 = z
+
+        if(lastSensorTimestamp != -1) {
+            double dt = (double)(event.timestamp-lastSensorTimestamp)/1.0e9;
+            for(int i = 0; i < 2; i++) {
+                if(Math.abs(event.values[i]) >  0.2){
+                    v[i] += (event.values[i])*dt;
+                }
+                v[i] *=0.95;
+
+
+                p[i] += v[i]*dt;
+                p[i]*=0.9;
+
+            }
+            web.setX((float)(-xdpm*p[0]));
+            web.setY((float)(-ydpm*p[1]));
+
+            System.out.println(dt +",("+ p[0] +","+ p[1] +"),("+v[0] +","+ v[1]+")");
+
+        }
 
         lastSensorTimestamp = event.timestamp;
     }
@@ -73,6 +111,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     protected void onResume() {
         super.onResume();
-        senSensorManager.registerListener(this, senAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        senSensorManager.registerListener(this, senAccelerometer, SensorManager.SENSOR_DELAY_FASTEST);
+        lastSensorTimestamp = -1;
     }
 }
