2b0ac71445f599f29905f9cb90ec3b4c1eff4faa
Cole Faust <colecfaust@gmail.com>
2017-01-21 11:04:04 -0800

    Added some display tests

--- app/src/main/java/com/SteadyView/SteadyView/MainActivity.java
+++ app/src/main/java/com/SteadyView/SteadyView/MainActivity.java
@@ -2,6 +2,7 @@ package com.SteadyView.SteadyView;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -16,5 +17,15 @@ public class MainActivity extends AppCompatActivity {
         web.getSettings().setJavaScriptEnabled(true);
         web.setWebViewClient(new WebViewClient());
         web.loadUrl("http://reddit.com");
+
+        DisplayMetrics metrics = getResources().getDisplayMetrics();
+        float inchesY = metrics.heightPixels / metrics.ydpi;
+        float inchesX = metrics.widthPixels / metrics.xdpi;
+        System.out.println("X pixels: "+metrics.widthPixels);
+        System.out.println("Y pixels: "+metrics.heightPixels);
+        System.out.println("X inches: "+inchesX);
+        System.out.println("Y inches: "+inchesY);
+        float metersX = inchesX * 0.0254f;
+        float metersY = inchesY * 0.0254f;
     }
 }
