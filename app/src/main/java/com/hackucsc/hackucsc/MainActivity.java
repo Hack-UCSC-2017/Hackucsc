73e27986e2711f8883da8119b6077dabcd309ed2
Cole Faust <colecfaust@gmail.com>
2017-01-21 02:04:08 -0800

    Reddit app. Plus removed .idea

--- app/src/main/java/com/hackucsc/hackucsc/MainActivity.java
+++ app/src/main/java/com/hackucsc/hackucsc/MainActivity.java
@@ -2,6 +2,9 @@ package com.hackucsc.hackucsc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        WebView web = (WebView)findViewById(R.id.webview);
+        web.getSettings().setJavaScriptEnabled(true);
+        web.setWebViewClient(new WebViewClient());
+        web.loadUrl("http://reddit.com");
     }
 }
