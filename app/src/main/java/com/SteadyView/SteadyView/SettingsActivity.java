15cc37e39e55b2bcb58410da5f79ce85792f250b
Jonathan <jojjang@ucsc.edu>
2017-01-22 02:22:27 -0800

    ray start the exit implementation

--- app/src/main/java/com/SteadyView/SteadyView/SettingsActivity.java
+++ app/src/main/java/com/SteadyView/SteadyView/SettingsActivity.java
@@ -3,6 +3,9 @@ package com.SteadyView.SteadyView;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 
 
 public class SettingsActivity extends AppCompatActivity {
@@ -11,6 +14,25 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings);
 
+        SeekBar seekBarX = (SeekBar) findViewById(R.id.ScaleX);
+        SeekBar seekBarY = (SeekBar) findViewById(R.id.ScaleY);
+
+
+
         System.out.println("inside settings button class boiiiiiis");
     }
+
+    public void settingsBtnClicked(View view) {
+        System.out.println("exit button Clicked AF");
+        //FIXME: Ray code here..
+    }
+
+//    private SeekBar.OnSeekBarChangeListener seekBarScaleListener =
+//            new SeekBar.OnSeekBarChangeListener() (
+//                @Override
+//                public void onProgressChanged(SeekBar seekBar, int progress,
+//                                              boolean fromUser){
+//
+//                }
+//            );
 }
