55f4aafdc257df45659902565a24f5d8212010f8
Cole Faust <colecfaust@gmail.com>
2017-01-21 22:14:45 -0800

    Fake VR mode

--- app/src/main/java/com/SteadyView/SteadyView/MainActivity.java
+++ app/src/main/java/com/SteadyView/SteadyView/MainActivity.java
@@ -12,7 +12,15 @@ import android.webkit.WebViewClient;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.vr.sdk.base.Eye;
+import com.google.vr.sdk.base.GvrActivity;
+import com.google.vr.sdk.base.GvrView;
+import com.google.vr.sdk.base.HeadTransform;
+import com.google.vr.sdk.base.Viewport;
+
+import javax.microedition.khronos.egl.EGLConfig;
+
+public class MainActivity extends GvrActivity implements GvrView.StereoRenderer {
 
     WebView web;
 
@@ -20,6 +28,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //enable VR
+        GvrView gvrView = (GvrView)findViewById(R.id.gvr_view);
+        gvrView.setRenderer(this);
+
+
         web = (WebView)findViewById(R.id.webview);
         final EditText urlbar = (EditText)findViewById(R.id.editText);
         web.getSettings().setJavaScriptEnabled(true);
@@ -31,10 +45,12 @@ public class MainActivity extends AppCompatActivity {
         };
         web.setWebViewClient(new MyClient());
         web.loadUrl("http://reddit.com");
+        web.bringToFront();
 
 
 
         urlbar.setText("http://reddit.com");
+        urlbar.bringToFront();
         urlbar.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView exampleView, int actionId, KeyEvent event) {
@@ -45,10 +61,25 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
         DisplayMetrics metrics = getResources().getDisplayMetrics();
         float inchesY = metrics.heightPixels / metrics.ydpi;
         float inchesX = metrics.widthPixels / metrics.xdpi;
         float metersX = inchesX * 0.0254f;
         float metersY = inchesY * 0.0254f;
     }
+
+
+    @Override
+    public void onNewFrame(HeadTransform headTransform) {}
+    @Override
+    public void onDrawEye(Eye eye) {}
+    @Override
+    public void onFinishFrame(Viewport viewport) {}
+    @Override
+    public void onSurfaceChanged(int i, int i1) {}
+    @Override
+    public void onSurfaceCreated(EGLConfig eglConfig) {}
+    @Override
+    public void onRendererShutdown() {}
 }
