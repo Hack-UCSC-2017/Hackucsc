1c9c03308d83f0fc2f81e96c1528f0b16b6c4b21
Jonathan <jojjang@ucsc.edu>
2017-01-21 16:29:38 -0800

    settings started

--- app/src/main/java/com/SteadyView/SteadyView/MainActivity.java
+++ app/src/main/java/com/SteadyView/SteadyView/MainActivity.java
@@ -6,6 +6,7 @@ import android.util.DisplayMetrics;
 import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.preference.PreferenceFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,10 +19,24 @@ public class MainActivity extends AppCompatActivity {
         web.setWebViewClient(new WebViewClient());
         web.loadUrl("http://reddit.com");
 
+
+
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content,
+                          new MainSettingsFragment()).commit();
+
         DisplayMetrics metrics = getResources().getDisplayMetrics();
         float inchesY = metrics.heightPixels / metrics.ydpi;
         float inchesX = metrics.widthPixels / metrics.xdpi;
         float metersX = inchesX * 0.0254f;
         float metersY = inchesY * 0.0254f;
+    }
+    public static class MainSettingsFragment extends PreferenceFragment{
+        @Override
+        public void onCreate(Bundle savedInstaanceState){
+            super.onCreate(savedInstaanceState);
+            System.out.println(R.xml.preferences);
+            addPreferencesFromResource(R.xml.preferences);
+        }
     }
 }
---
+++ app/src/main/java/com/SteadyView/SteadyView/Settings.java
@@ -0,0 +1,9 @@
+package com.SteadyView.SteadyView;
+
+
+import android.support.v7.app.AppCompatActivity;
+
+
+public class Settings extends AppCompatActivity {
+
+}
