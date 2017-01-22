4619d0548bf96e5d22c8f06a397143f9943d68ae
Jonathan <jojjang@ucsc.edu>
2017-01-21 19:24:50 -0800

    added google speech api

--- app/src/main/java/com/SteadyView/SteadyView/MainActivity.java
+++ app/src/main/java/com/SteadyView/SteadyView/MainActivity.java
@@ -1,7 +1,9 @@
 package com.SteadyView.SteadyView;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
@@ -14,9 +16,12 @@ import android.webkit.WebViewClient;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
-    WebView web;
+    public static WebView web;
+    private static final int SPEECH_REQUEST_CODE = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,5 +61,36 @@ public class MainActivity extends AppCompatActivity {
         float metersX = inchesX * 0.0254f;
         float metersY = inchesY * 0.0254f;
     }
-    
+
+    public void speechBtnClicked(View view) {
+        System.out.println("Hello World!");
+
+//        Intent speechIntent = new Intent(this, SpeechActivity.class);
+//        PendingIntent settingsPI = new PendingIntent(this, 0, settingsIntent, 0);
+//        startActivity(speechIntent);
+
+        displaySpeechRecognizer();
+    }
+
+    private void displaySpeechRecognizer() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        System.out.println("spokenText inside displatSpeechRecog");
+        startActivityForResult(intent, SPEECH_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        String spokenText = "";
+        if (requestCode == SPEECH_REQUEST_CODE && resultCode == RESULT_OK) {
+            List<String> results = data.getStringArrayListExtra(
+                    RecognizerIntent.EXTRA_RESULTS);
+            spokenText = results.get(0);
+        }
+        System.out.println("spokenText: " + spokenText);
+        web.loadUrl(spokenText);
+        super.onActivityResult(requestCode, resultCode, data);
+
+    }
 }
